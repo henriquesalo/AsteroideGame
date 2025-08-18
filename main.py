756cd1b45258b6eb688a34a4b05b13cd77af702c
henriquesalo <henriquersalomao@gmail.com>
2025-08-18 13:56:37 -0300

    firstCommitAsteroide

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+from player import Player
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        player.draw(screen)
+        
+        pygame.display.flip()
+
+        #Limitando o fps para 60 frames por segundo
+        dt = fps.tick(60) / 1000
+
+
+
+if __name__ == "__main__":
+    main()
