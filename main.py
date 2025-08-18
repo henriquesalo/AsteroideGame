ecc097c50add09d7a7c68132c0b6a8a47c5279da
henriquesalo <henriquersalomao@gmail.com>
2025-08-18 14:45:00 -0300

    Creating groups for better structure

--- main.py
+++ main.py
@@ -9,18 +9,24 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for objects in drawable:
+            objects.draw(screen)
 
         pygame.display.flip()
-
         #Limitando o fps para 60 frames por segundo
         dt = fps.tick(60) / 1000
 
