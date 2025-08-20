3caf6eb5114cbf9b965074d03093bae255434e9e
henriquesalo <henriquersalomao@gmail.com>
2025-08-20 14:36:12 -0300

    adding a game over messagem and sys to restart the game

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ def main():
 
         for asteroid in asteroids:
             if asteroid.check_collisions(player):
-                print("Game Over!")
-                sys.exit()
+                game_over(screen, score)
 
             for shot in shots:
                 if asteroid.check_collisions(shot):
@@ -59,6 +58,41 @@ def main():
 
         pygame.display.flip()
         dt = fps.tick(60) / 1000 # Limitando o fps para 60 frames por segundo
+
+def game_over(screen, score):
+    font_big = pygame.font.SysFont(pygame.font.get_default_font(), 80)
+    font_small = pygame.font.SysFont(pygame.font.get_default_font(), 40)
+
+    # Renderiza textos 
+    game_over_text = font_big.render("GAME OVER", True, (255, 0, 0))
+    score_text = font_small.render(f"Pontuação final: {score.score}", True, (255, 255, 255))
+    restart_text = font_small.render("Pressione ENTER para jogar novamente ou ESC para sair", True, (200, 200, 200))
+
+    # Pegando posições centralizadas
+    game_over_rect = game_over_text.get_rect(center=(screen.get_width() /2, screen.get_height() / 2 - 100))
+    score_rect = score_text.get_rect(center=(screen.get_width() / 2, screen.get_height() / 2))
+    restart_rect = restart_text.get_rect(center=(screen.get_width() / 2, screen.get_height() / 2 + 100))
+
+    # printando na tela
+    screen.fill((0, 0, 0))
+    screen.blit(game_over_text, game_over_rect)
+    screen.blit(score_text, score_rect)
+    screen.blit(restart_text, restart_rect)
+    pygame.display.flip()
+
+    # Loop de espera até usuário decidir
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:  # ENTER → reinicia
+                    Score.reset(score)
+                    main()
+                elif event.key == pygame.K_ESCAPE:  # ESC → sai
+                    pygame.quit()
+                    sys.exit()
 
 if __name__ == "__main__":
     main()
